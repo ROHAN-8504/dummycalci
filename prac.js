d8b3483838ebdd5fe6148e254d8d910e45840ccb
Rohan85041 <chrohankumar85041@gmail.com>
2026-04-22 15:45:19 +0530

    js example

---
+++ prac.js
@@ -0,0 +1,7 @@
+let days=["sun","mon","tues","wednes"]
+ let res= days.map(function(name){
+    return name+"day"
+  })
+  console.log(res)
+//expected output
+//["sunday","monday","tuesday","wednesday"]
